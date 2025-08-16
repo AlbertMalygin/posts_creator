b09a4bc493e3162d1bb62f835498971d71e2f6f4
Albert Malygin <albert.malygin@gmail.com>
2025-08-16 13:36:21 +0300

    Login page functionality added

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,36 @@
+import MyInput from "./UI/input/MyInput";
+import MyButton from "./UI/button/MyButton";
+import { useContext, useState } from "react";
+import { AuthContext } from "../context";
+
+export default function LoginForm() {
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  function login(e) {
+    e.preventDefault();
+    if (!userName || !userPassword) {
+      alert("Недостаточно данных для входа!");
+      return;
+    }
+    setIsAuth(true);
+  }
+  return (
+    <form className="login__form">
+      <h1>Авторизация</h1>
+      <MyInput
+        onChange={(e) => setUserName(e.target.value)}
+        value={userName}
+        placeholder="Введите логин..."
+      />
+      <MyInput
+        onChange={(e) => setUserPassword(e.target.value)}
+        value={userPassword}
+        placeholder="Введите пароль..."
+      />
+      <MyButton onClick={login}>Войти</MyButton>
+    </form>
+  );
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,9 @@
-import React, { useContext } from "react";
-import MyInput from "../components/UI/input/MyInput";
-import MyButton from "../components/UI/button/MyButton";
-import { AuthContext } from "../context";
+import LoginForm from "../components/LoginForm";
 
 export default function Login() {
-  const { isAuth, setIsAuth } = useContext(AuthContext);
-
-  function login(e) {
-    e.preventDefault();
-    setIsAuth(true);
-  }
   return (
     <div className="Login pageContent">
-      <form className="login__form">
-        <h1>Авторизация</h1>
-        <MyInput placeholder="Введите логин..." />
-        <MyInput placeholder="Введите пароль..." />
-        <MyButton onClick={login}>Войти</MyButton>
-      </form>
+      <LoginForm />
     </div>
   );
 }
